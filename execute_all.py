71a4a2df9ddc33af628b2fd8a55328e96245f0ed
Harry Parkes <harrydparkes@proton.me>
2024-10-08 20:55:20 +1300

    attempting to read in time data from executing each binary

--- execute_all.py
+++ execute_all.py
@@ -3,27 +3,32 @@ import pprint
 import toml
 import asyncio
 import subprocess
-import matplotlib
+# import matplotlib
 
 
 BUILD_SPECIFICATION_FILE = "build_specification.toml"
 
-map: dict[tuple[str, int, int], list[str]] = {}
+map: dict[tuple[str, int, int], tuple[float, float, float]] = {}
 
 
 def execution_command(file_name: str) -> list[str]:
-    return ["time", "-p", build_spec["directory"] + "/" + file_name]
+    return [
+        "time",
+        "--portability",
+        f"--output=txt/{file_name}.txt",
+        f"{build_spec['directory']}/{file_name}",
+    ]
 
 
-async def execute_binary(file_name: str) -> None:
-    program_name, thread_count, process_count = file_name.rstrip(
+async def time_binary_execution(file_name: str) -> None:
+    program_name, thread_count, process_count = file_name.removesuffix(
         build_spec["extension"]
     ).split(build_spec["delimeter"])
 
     binary_identifier = (
         program_name,
-        int(thread_count.rstrip("t")),
-        int(process_count.rstrip("p")),
+        int(thread_count.removesuffix("t")),
+        int(process_count.removesuffix("p")),
     )
 
     command = execution_command(file_name)
@@ -32,13 +37,25 @@ async def execute_binary(file_name: str) -> None:
     with open(build_spec["example_input_file"], "rb") as input_file:
         process_input = input_file.read()
 
-    result = subprocess.run(command, capture_output=True, input=process_input)
-    map[binary_identifier] = result.stderr.decode().splitlines()
+    # result = subprocess.run(command, capture_output=True, input=process_input)
+    result = subprocess.run(command, input=process_input)
+
+    # real, user, sys = result.stderr.decode().splitlines()
+    with open(f"txt/{file_name}.txt", "r") as output_file:
+        real, user, sys = output_file.read().splitlines()
+    print(real, user, sys)
+    real_time = float(real.removeprefix("real "))
+    user_time = float(user.removeprefix("user "))
+    sys_time = float(sys.removeprefix("sys "))
+
+    # print(real_time, user_time, sys_time)
+    map[binary_identifier] = (real_time, user_time, sys_time)
 
 
 async def main() -> None:
     tasks = [
-        execute_binary(file_name) for file_name in os.listdir(build_spec["directory"])
+        time_binary_execution(file_name)
+        for file_name in os.listdir(build_spec["directory"])
     ]
 
     await asyncio.gather(*tasks)
