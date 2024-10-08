c8fe9c63221107ba09fd7c649026eea4de0607e1
Harrison Parkes <hpa101@uclive.ac.nz>
2024-10-08 17:32:09 +1300

    execute_all.py now stores time information in a dictionary

--- execute_all.py
+++ execute_all.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 import toml
 import asyncio
 import subprocess
@@ -7,21 +8,32 @@ import matplotlib
 
 BUILD_SPECIFICATION_FILE = "build_specification.toml"
 
+map: dict[tuple[str, int, int], list[str]] = {}
+
 
 def execution_command(file_name: str) -> list[str]:
-    return ["time", build_spec["directory"] + "/" + file_name]
+    return ["time", "-p", build_spec["directory"] + "/" + file_name]
 
 
 async def execute_binary(file_name: str) -> None:
     program_name, thread_count, process_count = file_name.rstrip(
         build_spec["extension"]
     ).split(build_spec["delimeter"])
+
+    binary_identifier = (
+        program_name,
+        int(thread_count.rstrip("t")),
+        int(process_count.rstrip("p")),
+    )
+
     command = execution_command(file_name)
     print(*command)
+
     with open(build_spec["example_input_file"], "rb") as input_file:
         process_input = input_file.read()
+
     result = subprocess.run(command, capture_output=True, input=process_input)
-    print(result)
+    map[binary_identifier] = result.stderr.decode().splitlines()
 
 
 async def main() -> None:
@@ -29,7 +41,9 @@ async def main() -> None:
         execute_binary(file_name) for file_name in os.listdir(build_spec["directory"])
     ]
 
-    asyncio.gather(*tasks)
+    await asyncio.gather(*tasks)
+
+    pprint.pprint(map)
 
 
 if __name__ == "__main__":
