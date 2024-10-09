50bdf8d7fda126df10e60302520d8091616f6ecf
Harry Parkes <harrydparkes@proton.me>
2024-10-09 19:18:03 +1300

    correctly generating a graph for the process program and for the thread program

--- draw_graphs.py
+++ draw_graphs.py
@@ -9,16 +9,29 @@ def main() -> None:
         map: dict[ProgramName, dict[BinarySpecifier, TimeReading]]
         map = pickle.load(input_file)
 
+    # Thread program graph
     figure, axes = pyplot.subplots()
 
-    for program_name, execution_times in map.items():
-        for binary_specifier, time_reading in execution_times.items():
-            print(program_name, binary_specifier, time_reading)
+    x_axis = sorted([thread_count for thread_count, _ in map["thread"]])
+    y_axis = [map["thread"][(thread_count, 1)][0] for thread_count in x_axis]
 
-    x_axis = [thread_count for thread_count, _ in map["thread"]]
-    y_axis = [real for real, _, _ in map["thread"].values()]
+    axes.set_xlabel("Number of worker threads")
+    axes.set_ylabel("Execution time (seconds)")
     axes.plot(x_axis, y_axis)
-    pyplot.show()
+
+    figure.savefig("thread_graph.png")
+
+    # Process program graph
+    figure, axes = pyplot.subplots()
+
+    x_axis = sorted([process_count for _, process_count in map["process"]])
+    y_axis = [map["process"][(1, process_count)][0] for process_count in x_axis]
+
+    axes.set_xlabel("Maximum number of children processes")
+    axes.set_ylabel("Execution time (seconds)")
+    axes.plot(x_axis, y_axis)
+
+    figure.savefig("process_graph.png")
 
 
 if __name__ == "__main__":
